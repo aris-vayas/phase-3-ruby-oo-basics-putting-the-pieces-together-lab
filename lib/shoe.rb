b43f49e6a640258def924a24fd69db8d10eddb69
Aris Vayas <aris.vayas@gmail.com>
2022-05-17 17:08:29 -0400

    commit tree ce785d14e2884a21cb3c396c97fa9bbc6c144247

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Book 
+    attr_accessor :title, :author, :page, :genre
+    def initialize (title, author, page, genre)
+    @title=title
+    @author=author
+    @page=page
+    @genre=genre
+
+end
